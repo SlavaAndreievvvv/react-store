ea19be836d9e07ae41b384ea590bec521b5fb6c0
ANDREIEV VIACHESLAV <andreiev.viacheslav@gmail.com>
2023-05-17 13:30:34 +0300

    ✅ add categories to Sidebar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+import { getCategories } from "../../features/categories/categoriesSlice";
+
 import { Footer } from "../Footer";
 import { Header } from "../Header";
 import { Sidebar } from "../Sidebar";
 import { AppRoutes } from "../Routes/AppRoutes";
+import { useEffect } from "react";
 
 export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
+
   return (
     <div className="app">
       <Header />
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,28 @@
+import { useSelector } from "react-redux";
+
 import { NavLink } from "react-router-dom";
 import styles from "../../styles/Sidebar.module.css";
 
 export const Sidebar = () => {
+  const { list } = useSelector(({ categories }) => categories);
+
   return (
     <section className={styles.sidebar}>
       <div className={styles.title}>CATEGORIES</div>
-      <nav className={styles.nav}>
+      <nav>
         <ul className={styles.menu}>
-          <li className="">
-            <NavLink>Link</NavLink>
-          </li>
+          {list.map(({ id, name }) => (
+            <li key={id}>
+              <NavLink
+                className={({ isActive }) =>
+                  `${styles.link} ${isActive ? styles.active : ""}`
+                }
+                to={`/categories/${id}`}
+              >
+                {name}
+              </NavLink>
+            </li>
+          ))}
         </ul>
       </nav>
 
